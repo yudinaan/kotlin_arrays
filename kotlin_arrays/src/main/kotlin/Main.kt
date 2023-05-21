8147bfc9eeea666efacec2ff91fdf6b42b219aa6
yudinaan <yudinaanastasiya01@mail.ru>
2023-05-21 20:10:47 +0500

    workflow: 2 sorting an array

--- kotlin_arrays/src/main/kotlin/Main.kt
+++ kotlin_arrays/src/main/kotlin/Main.kt
@@ -1,16 +1,15 @@
-fun main(args: Array<String>) {
+fun main() {
+    print("Введите количество строк: ")
+    val n = input()
     print("Введите количество символов: ")
-    val length1 = readln()
-    val length = length1.toInt()
+    val length = input()
+    arraySort(n,length)
+}
 
-    val string: String = getRandomString(length)
-
-    val array = string.toCharArray()
-    print("Сгерерированная строка: ")
-    println(array)
-
-    val arraySort = array.sorted()
-    println("Отсортированная строка: $arraySort")
+fun input() : Int{
+    val n1 = readln()
+    val n = n1.toInt()
+    return n
 }
 
 fun getRandomString(length: Int) : String {
@@ -19,4 +18,33 @@ fun getRandomString(length: Int) : String {
         .map { allowedChars.random() }
         .joinToString("")
 }
+
+fun getArrayOfRandomString(n: Int, length: Int): Array<String> {
+    var array = Array<String>(n, {""})
+    print("Сгенерированный массив строк: ")
+    var i = 0
+    while (i <= n-1){
+        array[i] = getRandomString(length)
+        print(array[i])
+        print("\t")
+        i++
+    }
+    println()
+
+    return array
+}
+
+fun arraySort(n: Int, length: Int){
+    val array = getArrayOfRandomString(n, length)
+    val arraySort = array.sortedArray()
+    print("Отсортированный массив строк: ")
+    var i = 0
+    while (i <= n-1){
+        print(arraySort[i])
+        print("\t")
+        i++
+    }
+    println()
+}
+
 
